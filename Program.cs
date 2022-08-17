998b64af29748c7725f1a42417f29d15b5db1a5e
kocaeliproje <kocaeliproje@gmail.com>
2022-08-17 21:53:34 +0300

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int deger;
+        deger = 2;
+        Console.WriteLine(deger);
+    }
+}
